8e20b320ebe9cba86bd319f27d99e8826091e27f
Joe Martella <joem@microsoft.com>
2021-11-22 10:39:27 -0800

    we

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ setInterval(() => {
 function checkAvailability() {
   axios
     .get(
-      "https://redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1?key=ff457966e64d5e877fdbad070f276d18ecec4a01&tcin=82274909&store_id=1118&store_positions_store_id=1118&has_store_positions_store_id=true&zip=98053&state=WA&latitude=47.680&longitude=-122.020&scheduled_delivery_store_id=995&pricing_store_id=1118&has_pricing_store_id=true&is_bot=false&yy=2"
+      "https://redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1?key=ff457966e64d5e877fdbad070f276d18ecec4a01&tcin=82274909&store_id=1118&store_positions_store_id=1118&has_store_positions_store_id=true&zip=98034&state=WA&latitude=47.727959&longitude=-122.192733&scheduled_delivery_store_id=1118&pricing_store_id=1118&has_pricing_store_id=true&is_bot=false"
     )
     .then((res) => {
+      console.log(res?.status);
+      console.log(res?.data);
+
+      // check for stale token
+      if (res?.data?.errors) {
+        axios.post(
+          "https://discord.com/api/webhooks/911126327137021962/ADzfD7ab-aRprLNeUhQGaPDHS-rne0I4gxalrg5IiXc3MqgWMtKIrxvd57V0obRNpcEv",
+          {
+            content: `\`[${new Date().toLocaleTimeString()}]\` <@!514933935256371231> ${
+              res.data.errors[0].reason
+            }`,
+          }
+        );
+        console.log(
+          `[${new Date().toLocaleTimeString()}] ${res.data.errors[0].reason}`
+        );
+      }
+
       const isOutOfStock =
         res.data.data.product.fulfillment
           .is_out_of_stock_in_all_store_locations;
